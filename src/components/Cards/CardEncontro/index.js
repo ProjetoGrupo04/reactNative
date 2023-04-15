4628bffc2ef6aa968c76aa47982f5d4c5508dadc
Bruno Rodrigues <bruno.brd73@gmail.com>
2023-04-15 19:34:40 -0300

    Refactor: Alteração no componente do card

--- src/components/Cards/CardEncontro/index.js
+++ src/components/Cards/CardEncontro/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { View, Text, TouchableOpacity, StyleSheet, Image } from 'react-native';
 
-import { Feather } from '@expo/vector-icons';
-
 import { useNavigation } from '@react-navigation/native';
 
 function CardEncontro({ data }) {
@@ -13,45 +11,98 @@ function CardEncontro({ data }) {
     const name = data.descricao;
 
     return (
-        <TouchableOpacity onPress={() => navigation.navigate('SituacoesAprendizagensScreem', { id, name} )} >
-            <View style={styles.container}>
-                <Text style={styles.cardText}>{data.descricao}</Text>
-                <Text style={styles.cardText}>{data.data}</Text>
-                <Text style={styles.cardText}>{data.dia}</Text>
+
+        <View style={styles.container}>
+            <View style={styles.timeline}>
+                <View style={[styles.parte2,styles.shadowProp]}>
+                    <Text style={styles.titulo}>{data.descricao}</Text>
+                    <Text style={styles.titulo}>{data.data}</Text>
+                    <Text style={styles.descricao}>{data.dia}</Text>
+                    <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('SituacoesAprendizagensScreem', { id, name })}>
+                        <Text style={styles.detalhes}>Detalhe do encontro</Text>
+                    </TouchableOpacity>
+                </View>
+                {/* <View style={styles.parte2}>
+                       <Text style={styles.titulo}>{Moment(item.horaInicio).format('dddd')}
+                         , {Moment(item.horaInicio).format('L')}</Text>
+                       <Text style={styles.descricao}>{item.observacao}</Text>
+                       <Text style={styles.descricao}>Situação de aprendizagem</Text>
+                       <Text style={styles.descricao}>Objeto de aprendizagem</Text>
+                       <Text style={styles.descricao}>Atividade</Text>
+                       <TouchableOpacity style={styles.button}>
+                         <Text style={styles.detalhes}>Detalhe do encontro</Text>
+                       </TouchableOpacity>
+                     </View> */}
             </View>
-        </TouchableOpacity>
+        </View>
     );
 }
 
-const styles = StyleSheet.create({
-    container: {
-        width: 120,
-        height: 130,
-        backgroundColor: '#FFF',
-        margin: 5,
-        alignItems: 'center',
-        justifyContent: 'space-around',
-        borderRadius: 4,
-    },
-    card: {
-        alignItems: 'center',
-    },
-    cardImage: {
-        width: '90%',
-        height: 110,
-        borderRadius: 4,
-        marginBottom: 0,
-    },
-    cardText: {
-        color: '#000',
-        fontSize: 15,
-        fontWeight: 'bold',
-        textAlign: 'center',
-        elevation: 10
-    },
-})
-
 export default CardEncontro;
 
-
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: 'column',
+        paddingBottom: 10,
+        paddingLeft: 10,
+        paddingRight: 10,
+        alignItems: 'center',
+    },
+    timeline: {
+        flexDirection: 'row',
+        width: '100%',
+        marginTop: 10,
+        padding: 5,
+    },
+    tituloTimeline: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        marginBottom: 0,
+        justifyContent: 'center'
+    },
+    parte2: {
+        flex: 7,
+        padding: 15,
+        paddingBottom: 15,
+        backgroundColor: '#fff',
+        borderRadius: 10,
+        marginLeft: 0,
+        marginBottom: 0,
+        elevation: 10,
+    },
+    titulo: {
+        fontWeight: 'bold',
+        fontSize: 24,
+        color: "#092C4C",
+    },
+    descricao: {
+        fontSize: 20,
+        color: '#000',
+        marginTop: 10,
+    },
+    data: {
+        fontSize: 15,
+        color: '#656565',
+    },
+    button: {
+        backgroundColor: '#092C4C',
+        width: '100%',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 8,
+        padding: 20,
+        paddingTop: 10,
+        marginTop: 15,
+        marginBottom: 0,
+    },
+    detalhes: {
+        color: 'white',
+        fontWeight: 'bold',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 20,
+        paddingTop: 5,
+    },
+});
 
